0f23e670a06195467082d2110b581869940be5fe
Jacob <20714140+JacobPEvans-personal@users.noreply.github.com>
2026-05-31 00:13:28 +0000

    fix: uncomment required_providers and pin module version (#7) [issue-solver-2026-05-31]

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 terraform {
-#  required_providers {
-#    aws = {
-#      source = "hashicorp/aws"
-#    }
-#  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   backend "s3" {
     bucket  = "jacobpevans-tf-states"
     key     = "static-website"
@@ -20,7 +21,7 @@ provider "aws" {
 # Enable bucket versioning
 module "static-website" {
   source                  = "cloudmaniac/static-website/aws"
-#  version = "1.0.1"
+  version                 = "~> 1.0"
   website-domain-main     = "jacobpevans.com"
   website-domain-redirect = "www.jacobpevans.com"
 }
